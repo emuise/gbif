9f19e3e5bd45bcc00b3dc4b10b9b434280645b19
Evan Muise <55695611+emuise@users.noreply.github.com>
2026-07-04 17:38:19 -0700

    from other

--- scripts/1_download_global_gbif.R
+++ scripts/1_download_global_gbif.R
@@ -4,13 +4,21 @@ library(tidyverse)
 library(countrycode)
 library(geodata)
 library(tidyterra)
+library(duckdb)
 
 loc <- here::here("data", "gbif_global")
 
-# gbif_download(dir = loc)
+## download ALL gbif data
+# pak::pak("minioclient")
+# minioclient::install_mc()
+# rerun download code until it is done
+# gbifdb::gbif_download()
 
+## filter gbif data to north america
 # get north american countries
-noram <- vect("https://github.com/gbif/continents/raw/master/continent_cookie_cutter.gpkg") %>%
+noram <- vect(
+  "https://github.com/gbif/continents/raw/master/continent_cookie_cutter.gpkg"
+) %>%
   filter(continent_part == "north_america_east") # west is the aleutian islands, not a huge deal to cut them
 
 world <- geodata::world(path = here::here("data", "shps")) %>%
@@ -27,33 +35,52 @@ na_cc_iso2c <- countrycode(na_cc, origin = "iso3c", destination = "iso2c")
 
 drs <- fs::dir_ls(loc, recurse = T, type = "dir")
 
-link <- drs[length(drs)] %>%
-  open_dataset()
-
 ymin <- na_bbox$ymin
 ymax <- na_bbox$ymax
 xmin <- na_bbox$xmin
 xmax <- na_bbox$xmax
 
-na_gbif_db <- drs[length(drs)] %>%
+geo_issues <- c(
+  "COORDINATE_REPROJECTION_FAILED",
+  "COORDINATE_REPROJECTION_SUSPICIOUS",
+  "COORDINATE_UNCERTAINTY_METERS_INVALID",
+  "PRESUMED_NEGATED_LATITUDE",
+  "FOOTPRINT_WKT_MISMATCH",
+  "FOOTPRINT_WKT_INVALID",
+  "COUNTRY_COORDINATE_MISMATCH",
+  "COORDINATE_PRECISION_INVALID",
+  "PRESUMED_NEGATED_LONGITUDE",
+  "CONTINENT_COUNTRY_MISMATCH",
+  "CONTINENT_COORDINATE_MISMATCH",
+  "PRESUMED_SWAPPED_COORDINATE",
+  # this is a fix for the taxonomic backbone
+  # basically, if the parser cant find a perfect match to the submitted species ID, it may fuzzy match the species,
+  # this could lead to a confident GENUS
+  "TAXON_MATCH_HIGHERRANK"
+)
+
+## standard gbif cleaning procedures
+na_gbif_arrow <- drs[length(drs)] %>%
   open_dataset() %>%
-  to_duckdb() %>%
   filter(
     occurrencestatus == "PRESENT",
     !basisofrecord %in% c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN"),
     !is.na(species),
-    !is.na(decimallatitude), 
+    !is.na(decimallatitude),
     !is.na(decimallongitude),
     countrycode %in% na_cc_iso2c | is.na(countrycode),
-    decimallatitude < ymax, decimallatitude > ymin,
-    decimallongitude < xmax, decimallongitude > xmin
+    decimallatitude < ymax,
+    decimallatitude > ymin,
+    decimallongitude < xmax,
+    decimallongitude > xmin
   )
 
-unique_issues <- na_gbif_db %>%
-  mutate(flat_issue = sql("UNNEST(issue).array_element")) %>%
-  distinct(flat_issue) %>%
-  collect()
+clean_dir <- here::here("data", "gbif_noram")
+fs::dir_create(clean_dir)
 
+con <- dbConnect(duckdb())
+dbExecute(con, "SET preserve_insertion_order = false;")
+dbExecute(con, "SET threads = 4;") 
 # these are the standard geographic flags that are removed
 geo_issues <- c(
   "ZERO_COORDINATE",
@@ -78,18 +105,26 @@ geo_issues <- c(
   "TAXON_MATCH_HIGHERRANK"
 )
 
-# geospatial filtering based on the above 12 issues.
-# i am going to look through the issues again to find if i want to include any more
-# it is only filtering out about ~7 million points, which is simultaneously a lot and also not a lot
-# a lot in that it is 7 million observations gone, not a lot in that there are ~1.5 billion observations
-na_gbif_filtered <- na_gbif_db %>%
-  filter(
-    # Check that the filtered list size is 0 (meaning no blacklisted elements matched)
-    sql(sprintf(
-      "list_transform(list_filter(issue, x -> x.array_element IN %s), y -> y.array_element) = []",
-      # Converts the R character vector into a standard SQL ('A', 'B') string
-      paste0("(", paste(sprintf("'%s'", geo_issues), collapse = ", "), ")")
-    ))
-  )
+duckdb::duckdb_register_arrow(con, "raw_gbif_table", na_gbif_arrow)
 
-count(na_gbif_filtered)
+dbExecute(con, sprintf("
+  CREATE OR REPLACE VIEW test_filtered_view AS 
+  SELECT * EXCLUDE (issue) 
+  FROM (
+    SELECT * FROM raw_gbif_table
+  ) AS sampled_table
+  WHERE NOT list_has_any(
+    list_transform(issue, x -> x.array_element), 
+    CAST(%s AS VARCHAR[])
+  )
+  ORDER BY genus, species;
+", paste0("[", paste(sprintf("'%s'", geo_issues), collapse = ", "), "]")))
+
+message("Streaming sample dataset out to disk partitions...")
+dbExecute(con, sprintf("
+  COPY test_filtered_view 
+  TO '%s' 
+  (FORMAT 'PARQUET', PARTITION_BY 'genus', OVERWRITE_OR_IGNORE 1);
+", clean_dir))
+
+dbDisconnect(con, shutdown = TRUE)
